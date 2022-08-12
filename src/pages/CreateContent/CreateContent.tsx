13d5be5ab88760746e98c6bc984a4a190ed0f692
Mahmoud Ehab <mo.ehab150@gmail.com>
2022-08-13 01:34:42 +0200

    ExportsDiv has been added

--- src/pages/CreateContent/CreateContent.tsx
+++ src/pages/CreateContent/CreateContent.tsx
@@ -1,16 +1,20 @@
-import { useState } from "react";
-import useDataManager from "../../scripts/useDataManager";
-import loadFromHeader from "../../scripts/loadFromHeader";
+import { useState } from "react"
+import useDataManager from "../../scripts/useDataManager"
+import loadFromHeader from "../../scripts/loadFromHeader"
 
-import getStyle from "./style/MainStyle";
-import mediaQuery from "../../scripts/mediaQuery";
+import getStyle from "./style/MainStyle"
+import mediaQuery from "../../scripts/mediaQuery"
 
-import Navigation from "../../features/Navigation/Navigation";
-import SectionView from "./components/SectionView";
-import HeadlineView from "./components/HeadlineView";
-import TopicView from "./components/TopicView";
-import Button from "../../MiniComponents/Button";
-import exportAsJSON from "../../scripts/Exports/exportAsJSON";
+import { useAnimate } from 'react-animation-maker'
+import { SlideInDown, SlideOutDown } from 'react-animation-maker'
+
+import Navigation from "../../features/Navigation/Navigation"
+import SectionView from "./components/SectionView"
+import HeadlineView from "./components/HeadlineView"
+import TopicView from "./components/TopicView"
+import Button from "../../MiniComponents/Button"
+import exportAsJSON from "../../scripts/Exports/exportAsJSON"
+import exportAsPDF from "../../scripts/Exports/exportAsPDF"
 
 
 const CreateContent = () => {
@@ -20,6 +24,7 @@ const CreateContent = () => {
 
     const [navVisibility, setNavVisibility] = useState(true);
     const [firstRender, setFirstRender] = useState(true);
+    const [ExportsDiv, setExportsAnim] = useAnimate({from: SlideInDown.from, to: []});
 
     const showNav = () => {
         setNavVisibility(true);
@@ -29,8 +34,8 @@ const CreateContent = () => {
             setNavVisibility(false);
     }
 
-    const exportData = () => {
-        exportAsJSON(DM);
+    const showExportsBtns = () => {
+        setExportsAnim(SlideInDown);
     }
 
     // Load json file, if a fileURL is given in window.location
@@ -46,10 +51,10 @@ const CreateContent = () => {
                 onShow={() => showNav()}
                 hidden={!navVisibility}>
                 <Button 
-                    mainStyle={style.exportBtn.default} 
-                    hoverStyle={style.exportBtn.hover}
+                    mainStyle={style.exportBtn('#10ac84').default} 
+                    hoverStyle={style.exportBtn('').hover}
                     label={"Export"} 
-                    onClick={() => exportData()}
+                    onClick={() => showExportsBtns()}
                 />  
             </Navigation> 
 
@@ -65,6 +70,27 @@ const CreateContent = () => {
                     <TopicView DM={DM} />
                 }
             </div>
+
+            <ExportsDiv style={style.exportsDiv}>
+                <Button 
+                    mainStyle={style.exportBtn('#242424', '250px').default} 
+                    hoverStyle={style.exportBtn('').hover}
+                    label={"JSON"} 
+                    onClick={() => exportAsJSON(DM.getDataAsJSON())}
+                />  
+                <Button 
+                    mainStyle={style.exportBtn('#af0000', '250px').default} 
+                    hoverStyle={style.exportBtn('').hover}
+                    label={"PDF"} 
+                    onClick={() => exportAsPDF(DM)}
+                /> 
+                <Button 
+                    mainStyle={style.exportBtn('#000000').default} 
+                    hoverStyle={style.exportBtn('').hover}
+                    label={"Close"} 
+                    onClick={() => setExportsAnim(SlideOutDown)}
+                /> 
+            </ExportsDiv>
         </div>
     );
 }
--- src/pages/CreateContent/style/MainStyle.ts
+++ src/pages/CreateContent/style/MainStyle.ts
@@ -17,22 +17,35 @@ const getStyle = () => ({
     backgroundColor: '#f1f1f1',
   },
 
-  exportBtn : {
+  exportsDiv: {
+    display: 'flex',
+    flexFlow: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute' as 'absolute',
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#00000022',
+  },
+
+  exportBtn: (bgcolor?: string, width?: string) => ({
     default: {
+      width: width || 'auto',
+      margin: '10px 0',
       padding: 20,
       border: 0,
       borderRadius: 5,
       boxShadow: '0 0 5px 1px #00000011',
       fontSize: '1em',
       color: '#fff',
-      backgroundColor: '#10ac84',
+      backgroundColor: bgcolor || '#000000',
       transition: '250ms',
       cursor: 'pointer',
     },
     hover: {
-      backgroundColor: '#1dd1a1',
+      boxShadow: '0 0 10px 2px #00000044',
     },
-  },
+  }),
 });
 
 export default getStyle;
