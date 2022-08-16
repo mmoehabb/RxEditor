a67ee3eb7ee8156782bff314127d15236e3d9596
Mahmoud Ehab <mo.ehab150@gmail.com>
2022-08-16 16:19:46 +0200

    additional tools have been added

--- src/features/TextEditor/tools/ButtonsObject.ts
+++ src/features/TextEditor/tools/ButtonsObject.ts
@@ -1,21 +1,37 @@
 import { 
   BsFillBrushFill,
-  BsFonts, 
   BsListOl, 
   BsListUl, 
   BsTypeBold,
   BsTypeItalic,
   BsLink45Deg,
   BsImage,
-  BsPlusSquareDotted
+  BsPlusSquareDotted,
+  BsCodeSlash
 } from "react-icons/bs";
+
+import { FaHeading } from "react-icons/fa"
+import { 
+  RiHeading, 
+  RiDoubleQuotesR 
+} from "react-icons/ri"
+import { CgFormatHeading } from "react-icons/cg"
+import { VscNote } from "react-icons/vsc"
 
 import { add, addTagWithAttributes } from "../functions/NodeFunctions";
 
 export const buttonsData = [
   {
-    func: () => add("h1", "Header", true),
-    img: BsFonts,
+    func: () => add("h1", "Header 1"),
+    img: FaHeading,
+  },
+  {
+    func: () => add("h2", "Header 2"),
+    img: RiHeading,
+  },
+  {
+    func: () => add("h3", "Header 3"),
+    img: CgFormatHeading,
   },
   {
     func: () => add("strong", "", true),
@@ -26,16 +42,28 @@ export const buttonsData = [
     img: BsTypeItalic,
   },
   {
-    func: () => add("ol", "<li></li>", false),
+    func: () => add("code", "", true),
+    img: BsCodeSlash,
+  },
+  {
+    func: () => add("ol", "<li></li>"),
     img: BsListOl,
   },
   {
-    func: () => add("ul", "<li></li>", false),
+    func: () => add("ul", "<li></li>"),
     img: BsListUl,
   },
   {
     func:() => add("mark", "", true),
     img: BsFillBrushFill,
+  },
+  {
+    func:() => add("q", "", true),
+    img: RiDoubleQuotesR,
+  },
+  {
+    func:() => add("note", "", true),
+    img: VscNote,
   },
   {
     func:() => addTagWithAttributes("a", ["href"]),
